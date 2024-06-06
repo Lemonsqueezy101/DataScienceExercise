072da28cc813c9d40b35bc9bbc76716324f72fe2
Clemens Krengel <clemens.krengel@stud.uni-goettingen.de>
2024-06-06 13:28:08 +0200

    cleaned mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,50 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+import matplotlib.pyplot as plt
 
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+# Sortieralgorithmus
+def mergeSort(list):
+    
+    # Bedingung für den rekursiven Aufruf
+    if (len(list) > 1):
+        
+        mid = len(list) // 2
+        left = list[:mid]
+        right = list[mid:]
 
         mergeSort(left)
         mergeSort(right)
 
-        l = 0
-        r = 0
-        i = 0
+        l, r, i = 0
 
+        # Zusammenfuegen für den Mergesort
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                list[i] = left[l]
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                list[i] = right[r]
                 r += 1
             i += 1
 
         while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
+            list[i] = left[l]
+            l, i += 1
 
         while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
+            list[i] = right[r]
+            r, i += 1
 
-
-import matplotlib.pyplot as plt
-
+# Definition der Liste
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+
+# Plotten der originellen Liste
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
+
+# Auführen des Sortieralgorithmus
 mergeSort(my_list)
+
+# Plotten der Ergebnisse
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
