86204bd2dc1123da936900cb7775322a99b23f15
Clemens Krengel <clemens.krengel@stud.uni-goettingen.de>
2024-06-06 13:52:50 +0200

    refactored mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,60 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+import matplotlib.pyplot as plt
 
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+# Sortieralgorithmus
+def mergeSort(list):
+    
+    # Bedingung für den rekursiven Aufruf
+    if (len(list) > 1):
+        
+        mid = len(list) // 2
+        left = list[:mid]
+        right = list[mid:]
 
         mergeSort(left)
         mergeSort(right)
 
-        l = 0
-        r = 0
-        i = 0
+        l, r, i = 0, 0, 0
 
+        # Zusammenfuegen für den Mergesort
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                list[i] = left[l]
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                list[i] = right[r]
                 r += 1
             i += 1
 
         while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
+            list[i] = left[l]
+            l += 1 
             i += 1
 
         while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
+            list[i] = right[r]
             r += 1
             i += 1
 
+def plotting(my_list, title):
+    x = range(len(my_list))
+    bars = plt.bar(x,my_list)
+    plt.xlabel("Index")
+    plt.ylabel("Value")
+    plt.title(title)
+    for bar in bars:
+        height = bar.get_height()  # Höhe des Balkens
+        plt.text(bar.get_x() + bar.get_width() / 2, height, str(height), ha='center', va='bottom')
+    plt.xticks(x)
+    plt.show()
 
-import matplotlib.pyplot as plt
-
+# Definition der Liste
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+# Plotten der originellen Liste
+plotting(my_list, "Unsorted")
+
+# Auführen des Sortieralgorithmus
 mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+# Plotten der Ergebnisse
+plotting(my_list, "Sorted")
